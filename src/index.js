80e71571b9a9edf073c4fd7a1efd1509cf875f9e
DevAlexeyProtasenya <283fg56j98@gmail.com>
2021-03-02 16:12:59 +0300

    feat: add brackets checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = false;
+  for (i = 0; i < str.length; i++) {
+    bracketsConfig.forEach(element => {
+      if (element[1] === str[i]) {
+        if (element[0] === str[i - 1]) {
+          str = str.substr(0, i - 1) + str.substr(i + 1);
+          i -= 2;
+        }
+      }
+    });
+  }
+  if (str === '') {
+    result = true;
+  }
+  return result;
 }
